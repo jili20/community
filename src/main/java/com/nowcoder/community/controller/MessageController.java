4a7b0412aa1f3e2b56c3a760241c077a5516bcf4
Bing <bing_yu2001@163.com>
2020-05-12 14:41:22 +0800

    显示私信详情

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -9,6 +9,7 @@ import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ public class MessageController {
     private UserService userService;
 
     //私信列表
-    @RequestMapping(path = "/letter/list",method = RequestMethod.GET)
+    @RequestMapping(path = "/letter/list",method = RequestMethod.GET) // 方法的返回路径
     public String getLetterList(Model model, Page page){
         User user = hostHolder.getUser();
         //分页信息
@@ -62,6 +63,45 @@ public class MessageController {
         return "/site/letter";
     }
 
+    // 私信详情页面
+    @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model){
+        //分页信息
+        page.setLimit(5);//每页显示5条
+        page.setPath("/letter/detail/" + conversationId); // 当前路径
+        page.setRows(messageService.findLetterCount(conversationId));//设置行数
+        //私信列表
+        List<Message> letterList = messageService.findLetters(conversationId, page.getOffset(), page.getLimit());
+        List<Map<String,Object>> letters = new ArrayList<>();
+        if (letterList != null) {
+            for (Message message : letterList) {
+                Map<String,Object> map = new HashMap<>();
+                map.put("letter", message);
+                map.put("fromUser", userService.findUserById(message.getFromId()));
+                letters.add(map);
+
+            }
+        }
+        model.addAttribute("letters", letters);
+        //查询私信目标，发送给模板显示
+        model.addAttribute("target",getLetterTarget(conversationId));
+
+        return "/site/letter-detail";
+    }
+
+
+    // 私信目标
+    private User getLetterTarget(String conversationId){
+        String[] ids = conversationId.split("_");//拆分成两个id
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+
+        if (hostHolder.getUser().getId() == id0) {
+            return userService.findUserById(id1);
+        }else {
+            return userService.findUserById(id0);
+        }
+    }
 }
 
 
